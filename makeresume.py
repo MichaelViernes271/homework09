31d8ae54f97c8b6bdaf9ed7aa9961d4d7c2e90a4
Michael Viernes <92641515+MichaelViernes271@users.noreply.github.com>
2022-02-11 15:00:46 +0800

    First run with top profile (name, desc, & skills
    commit 7af00d7c15e8f925d4095c61d92b963f4c75edfe
    Author: MichaelViernes271 <michaeleviernes@iskolarngbayan.pup.edu.ph>
    Date:   Fri Feb 11 14:22:08 2022 +0800
    
         On branch main
         Changes to be committed:
                modified:   makeresume.py
                modified:   person.json
    
        Description:
         (1). Added more profile including the background information of the user as well as the json file where "accolades" 
    
    & "publications" are listed too.
         (2). Tested the program but sometimes fail. Currently debugging.

---
+++ makeresume.py
@@ -0,0 +1,41 @@
+# NOTE: install 'python pip install fpdf2'
+import json
+from fpdf import FPDF
+
+
+# json file casting
+person = None
+with open('person.json') as p:
+    person = json.load(p)    
+
+# initialize FPDF library to be used
+pdf = FPDF('p', 'cm', 'Letter')
+pdf.add_page()
+pdf.set_margin(2)
+pdf.set_font('times', '', 1)
+
+# my top profile
+pdf.set_font('times', 'B', 20)
+pdf.cell(0, 2, person['name'], ln=1)
+pdf.cell(0, 1, person['role'])
+pdf.ln(1)
+pdf.cell(20) # padding my image
+pdf.image('michael.JPG', 16,2.25,3)
+
+# profile description
+pdf.set_font('times', 'B', 14)
+pdf.cell(0, 1, 'Description:', ln=1)
+pdf.set_font('times', '', 12) # for the desciption
+pdf.multi_cell(0,1, person['desc'], ln=1)
+
+# my contacts
+pdf.set_font('times', 'B', 14)
+pdf.cell(0, 1, 'Contacts:', ln=1)
+pdf.set_font('times', '', 12)
+pdf.cell(6,1, "Phone: " + person['phone'])
+pdf.cell(6,1, "E-mail: " + person['email'], ln=1)
+pdf.cell(0,1, "Linkedin: " + person['linkedin'], ln=1)
+
+
+# make resume
+pdf.output('VIERNES_MICHAEL.pdf')
